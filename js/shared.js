e0bc140e5ed6b79b0805edfce5a31535facf5191
Corbin Davenport <me@corbin.io>
2022-08-05 00:13:15 -0400

    Update detection code

--- js/shared.js
+++ js/shared.js
@@ -101,10 +101,7 @@ Object.entries(localStorage).forEach(function(key) {
 })
 
 // Hide donation links on Android APK
-if ((localStorage['android-app'] === 'true') || document.referrer.includes('android-app://')) {
+if (document.referrer.includes('android-app://')) {
     console.log('Android app detected')
     document.querySelector('.donate-card').classList.add('d-none')
-    if (!(localStorage['android-app'] === 'true')) {
-        localStorage['android-app'] = 'true'
-    }
 }
